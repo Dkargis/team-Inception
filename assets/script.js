295309c1c31c787331bd35297040d644016204e1
Dane Kargis <dane.kargis@gmail.com>
2023-03-30 20:26:24 -0600

    added some dropdowns and figuring out the style and stuff

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+var APIkeyopenweather = "2b43e2e1e9e4752f4b3c2320f365c4f8"
+var APIkeyNPS = "xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg"
+var lat, lon;
+var FiveDayForecastEl = document.getElementById("Five-Day-Forecast");
