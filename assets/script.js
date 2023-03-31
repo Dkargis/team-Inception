8ebf38330461dc3162ec3764f9135f41f208ac33
Dane Kargis <dane.kargis@gmail.com>
2023-03-30 21:04:59 -0600

    Basic Framwork and Javascript V1

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,25 @@
 var APIkeyopenweather = "2b43e2e1e9e4752f4b3c2320f365c4f8"
 var APIkeyNPS = "xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg"
-var lat, lon;
+var ZionEl = document.getElementById("Zion");
+var BryceEl = document.getElementById("Bryce");
+var ArchesEl = document.getElementById("Arches");
+var CanyonlandsEl = document.getElementById("Canyonlands");
+var CapitolReefEl = document.getElementById("Capitol-Reef")
+
+/* 
+    The following arrays contain the longitudes and latituds for Utah's National Parks.
+    Index Key: 0 = Zion, 1 = Bryce Canyon, 2 = Arches, 3 = Canyonlands, 4 = Capitol Reef
+*/
+var lat = [37.2982, 37.5930, 38.7331, 38.2136, 38.0877]
+var lon = [113.0263, 112.1871, 109.5925, 109.9025, 111.1355]
 var FiveDayForecastEl = document.getElementById("Five-Day-Forecast");
+var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkeyopenweather;
+var NPStrailsURL = ""
+
+function getNationalpark {
+    if (event.target === ZionEl)
+}
+
+function getWeatherparks(weatherURL) {
+    fetch(weatherURL)
+}
