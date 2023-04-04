8047e77e61b58b3298327c67823451c16584251b
Dane Kargis <dane.kargis@gmail.com>
2023-04-03 21:20:45 -0600

    The three of us collabed implemting the api that will give us the trails for the national parks in utah

--- assets/script.js
+++ assets/script.js
@@ -17,28 +17,46 @@ var lat = [37.2982, 37.5930, 38.7331, 38.2136, 38.0877]
 var lon = [113.0263, 112.1871, 109.5925, 109.9025, 111.1355]
 var FiveDayForecastEl = document.getElementById("Five-Day-Forecast");
 var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkeyopenweather;
-var NPStrailsURL = ""
+var NPStrailsURL = "https://developer.nps.gov/api/v1/amenities/parksplaces?q=trails&api_key=xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg&id=39BE9614-4284-4B29-A650-AB2E7215C58B&parkCode=" + "arch,care,zion,cany,brca"
 
-NationalParkDropdownEl.addEventListener("click", function(event) {
-    event.preventDefault();
-    if (event.target === ZionEl ){
-        lat = [0];
-        lon= [0]
-    } else if (event.target === BryceEl) {
-        lat = [1];
-        lon = [1]
-    } else if (event.target === ArchesEl) {
-        lat = [2];
-        lon = [2]
-    } else if (event.target === CanyonlandsEl) {
-        lat = [3];
-        lon = [3]
-    } else if (event.target === CapitolReefEl) {
-        lat = [4];
-        lon = [4]
-    } 
-} )
+// NationalParkDropdownEl.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     if (event.target === ZionEl ){
+//         lat = [0];
+//         lon= [0]
+//     } else if (event.target === BryceEl) {
+//         lat = [1];
+//         lon = [1]
+//     } else if (event.target === ArchesEl) {
+//         lat = [2];
+//         lon = [2]
+//     } else if (event.target === CanyonlandsEl) {
+//         lat = [3];
+//         lon = [3]
+//     } else if (event.target === CapitolReefEl) {
+//         lat = [4];
+//         lon = [4]
+//     } 
+// } )
 
 function getWeatherparks(weatherURL) {
     fetch(weatherURL)
 }
+
+
+function getNPStrails() {
+    fetch(NPStrailsURL)
+    .then(function(response) {
+        return response.json();
+    })
+   .then (function(data) {
+       console.log(data)
+
+   })
+    .catch(function(error) {
+        console.log(error)
+    })
+
+}
+
+getNPStrails();
