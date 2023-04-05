223b3109d79c37d1f4ceacb8f9d2d98758085c1e
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-04-04 19:12:30 -0600

    uncommented the if statements on the national park dropdownEl for the event listener to try and grab the proper lat and lon to put in the getweather function

--- assets/script.js
+++ assets/script.js
@@ -18,29 +18,41 @@ var lon = [113.0263, 112.1871, 109.5925, 109.9025, 111.1355]
 var FiveDayForecastEl = document.getElementById("Five-Day-Forecast");
 var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkeyopenweather;
 var NPStrailsURL = "https://developer.nps.gov/api/v1/amenities/parksplaces?q=trails&api_key=xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg&id=39BE9614-4284-4B29-A650-AB2E7215C58B&parkCode=" + "arch,care,zion,cany,brca"
+//var NPStrailsURL = "https://developer.nps.gov/api/v1/activities/parks?q=hiking&limit=50&api_key=xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg"
+//var NPStrailsURL = "https://developer.nps.gov/api/v1/activities/parks?q=trailhead&limit=50&api_key=xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg"
 
-// NationalParkDropdownEl.addEventListener("click", function(event) {
-//     event.preventDefault();
-//     if (event.target === ZionEl ){
-//         lat = [0];
-//         lon= [0]
-//     } else if (event.target === BryceEl) {
-//         lat = [1];
-//         lon = [1]
-//     } else if (event.target === ArchesEl) {
-//         lat = [2];
-//         lon = [2]
-//     } else if (event.target === CanyonlandsEl) {
-//         lat = [3];
-//         lon = [3]
-//     } else if (event.target === CapitolReefEl) {
-//         lat = [4];
-//         lon = [4]
-//     } 
-// } )
+// created an event listener for the dropdown menu to find the lat and lon for the selected park and put them into the weatherURL
+ NationalParkDropdownEl.addEventListener("click", function(event) {
+    event.preventDefault();
+    function getPark (event) {
+    if (event.target === ZionEl ){
+        lat = [0];
+         lon= [0]
+    } else if (event.target === BryceEl) {
+        lat = [1];
+        lon = [1];
+    } else if (event.target === ArchesEl) {
+         lat = [2];
+         lon = [2]
+     } else if (event.target === CanyonlandsEl) {
+        lat = [3];
+         lon = [3]
+     } else if (event.target === CapitolReefEl) {
+         lat = [4];
+         lon = [4]
+     } 
+        getWeatherparks(weatherURL);
+ } )
 
 function getWeatherparks(weatherURL) {
     fetch(weatherURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        console.log(data)
+    }
+    )
 }
 
 
