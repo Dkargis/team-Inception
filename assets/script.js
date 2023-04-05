a05f4d685a3682fd69f2b9c0d49910ab341cad46
Tyler Mifflin <tylermifflin@Tylers-MacBook-Air.local>
2023-04-04 21:07:34 -0600

    day 1 of 5 weather is done

---
+++ assets/script.js
@@ -0,0 +1,120 @@
+// created a variable to for the weather API key and the NPS API key, and the elements for the dropdown menu
+var APIkeyopenweather = "2b43e2e1e9e4752f4b3c2320f365c4f8";
+var APIkeyNPS = "xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg";
+var NationalParkDropdownEl = document.querySelector(".National-Park-Dropdown");
+
+var parkSelectEl = document.querySelector(".Park-Option");
+
+var ZionEl = document.getElementById("Zion");
+var BryceEl = document.getElementById("Bryce");
+var ArchesEl = document.getElementById("Arches");
+var CanyonlandsEl = document.getElementById("Canyonlands");
+var CapitolReefEl = document.getElementById("Capitol-Reef")
+var fivedayforecast1El = document.getElementById("fivedayforecast1");
+var fivedayforecast2El = document.getElementById("fivedayforecast2");
+var fivedayforecast3El = document.getElementById("fivedayforecast3");
+var fivedayforecast4El = document.getElementById("fivedayforecast4");
+var fivedayforecast5El = document.getElementById("fivedayforecast5");
+
+/*
+    The following arrays contain the longitudes and latituds for Utah's National Parks.
+    Index Key: 0 = Zion, 1 = Bryce Canyon, 2 = Arches, 3 = Canyonlands, 4 = Capitol Reef
+*/
+var lat =   [37.2982, 37.5930, 38.7331, 38.2136, 38.0877]
+var lon = [-113.0263, -112.1871, -109.5925, -109.9025, -111.1355]
+var FiveDayForecastEl = document.getElementById("Five-Day-Forecast");
+var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkeyopenweather;
+var NPStrailsURL = "https://developer.nps.gov/api/v1/amenities/parksplaces?q=trails&api_key=xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg&id=39BE9614-4284-4B29-A650-AB2E7215C58B&parkCode=" + "arch,care,zion,cany,brca"
+//var NPStrailsURL = "https://developer.nps.gov/api/v1/activities/parks?q=hiking&limit=50&api_key=xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg"
+//var NPStrailsURL = "https://developer.nps.gov/api/v1/activities/parks?q=trailhead&limit=50&api_key=xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg"
+
+// created an event listener for the dropdown menu to find the lat and lon for the selected park and put them into the weatherURL
+ //NationalParkDropdownEl.addEventListener("click", function(event) {
+    //event.preventDefault();
+    //function getPark (event) {
+    //if (event.target === ZionEl ){
+        //lat = [0];
+         //lon= [0]
+    //} else if (event.target === BryceEl) {
+       // lat = [1];
+      //  lon = [1];
+   // } else if (event.target === ArchesEl) {
+       //  lat = [2];
+       //  lon = [2]
+    // } else if (event.target === CanyonlandsEl) {
+      //  lat = [3];
+      //   lon = [3]
+    // } else if (event.target === CapitolReefEl) {
+      //   lat = [4];
+     //    lon = [4]
+   //  } 
+   //     getWeatherparks(weatherURL);
+ //} 
+
+function getWeatherparks(weatherURL) {
+    fetch(weatherURL)
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        // created variables for name of park, date, weather icon, temperature, humidity, wind speed, and UV index to grab a 5 day forecast
+        var date = data.list[0].dt_txt;
+        date= date.split(" ");
+        date= date[0];
+        var weatherIcon = data.list[0].weather[0].icon;
+        var temperature = data.list[0].main.temp;
+        temperature = (temp - 273.15) * 1.80 + 32;
+        temperature = Math.round(temperature);
+        var humidity = data.list[0].main.humidity;
+        var windSpeed = data.list[0].wind.speed;
+        windSpeed = Math.round(windSpeed);
+        var UVindex = data.list[0].main.uvi;
+        // console logged the data to make sure it was working
+        console.log(data);
+        console.log(date);
+        console.log(weatherIcon);
+        console.log(temperature);
+        console.log(humidity);
+        console.log(windSpeed);
+        console.log(UVindex);
+        // created elements to display the 5 day forecast 
+        var dateEl = document.createElement("p");
+        dateEl.textContent = date;
+        fivedayforecast1El.appendChild(dateEl);
+        var weatherIconEl = document.createElement("img");
+        weatherIconEl.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon + ".png");
+        fivedayforecast1El.appendChild(weatherIconEl);
+        var temperatureEl = document.createElement("p");
+        temperatureEl.textContent = "Temperature: " + temperature + "°F";
+        fivedayforecast1El.appendChild(temperatureEl);
+        var humidityEl = document.createElement("p");
+        humidityEl.textContent = "Humidity: " + humidity + "%";
+        fivedayforecast1El.appendChild(humidityEl);
+        var windSpeedEl = document.createElement("p");
+        windSpeedEl.textContent = "Wind Speed: " + windSpeed + " MPH";
+        fivedayforecast1El.appendChild(windSpeedEl);
+        var UVindexEl = document.createElement("p");
+        UVindexEl.textContent = "UV Index: " + UVindex;
+        fivedayforecast1El.appendChild(UVindexEl);
+
+    })     
+    }
+
+
+
+
+//function getNPStrails() {
+  //  fetch(NPStrailsURL)
+ //   .then(function(response) {
+  //      return response.json();
+ //   })
+ //  .then (function(data) {
+  //     console.log(data)
+
+ //  })
+ //   .catch(function(error) {
+  //      console.log(error)
+ //   })
+//}
+ 
+//getNPStrails();
