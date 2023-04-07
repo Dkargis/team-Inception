cd70e76687d5510c3a8bcf61e304667baaa51b18
tylermifflin <123903709+tylermifflin@users.noreply.github.com>
2023-04-06 19:15:18 -0600

    Update script.js
    I added my getWeatherparks function from my personal branch and called it to test it, I also added variables for each day of the 5 day forecast to use them to create elements and append the weather for each day.

--- assets/script.js
+++ assets/script.js
@@ -15,16 +15,19 @@ var CanyonlandsEl = document.getElementById("Canyonlands");
 var CapitolReefEl = document.getElementById("Capitol-Reef")
 var FavoritesBTN = document.getElementById("Favorites-BTN");
 var FavoritesListEl = document.getElementById("Favorites");
+var fivedayforecast1El = document.getElementById("fivedayforecast1");
+var fivedayforecast2El = document.getElementById("fivedayforecast2");
+var fivedayforecast3El = document.getElementById("fivedayforecast3");
+var fivedayforecast4El = document.getElementById("fivedayforecast4");
+var fivedayforecast5El = document.getElementById("fivedayforecast5");
 
+   // The following arrays contain the longitudes and latituds for Utah's National Parks.
+   // Index Key: 0 = Zion, 1 = Bryce Canyon, 2 = Arches, 3 = Canyonlands, 4 = Capitol Reef
 
-/* 
-    The following arrays contain the longitudes and latituds for Utah's National Parks.
-    Index Key: 0 = Zion, 1 = Bryce Canyon, 2 = Arches, 3 = Canyonlands, 4 = Capitol Reef
-*/
 var lat =   [37.2982, 37.5930, 38.7331, 38.2136, 38.0877]
 var lon = [-113.0263, -112.1871, -109.5925, -109.9025, -111.1355]
 var FiveDayForecastEl = document.getElementById("Five-Day-Forecast");
-var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIkeyopenweather;
+var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + 37.2982 + "&lon=" + -113.0263 + "&appid=" + APIkeyopenweather;
 var NPStrailsURL = "https://developer.nps.gov/api/v1/amenities/parksplaces?q=trails&api_key=xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg&id=39BE9614-4284-4B29-A650-AB2E7215C58B&parkCode=" + "arch,care,zion,cany,brca"
 //var NPStrailsURL = "https://developer.nps.gov/api/v1/activities/parks?q=hiking&limit=50&api_key=xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg"
 //var NPStrailsURL = "https://developer.nps.gov/api/v1/activities/parks?q=trailhead&limit=50&api_key=xzXARQmOI9aGk4SgOgRskz4plWhHvCdtI3NcKsJg"
@@ -52,30 +55,162 @@ var NPStrailsURL = "https://developer.nps.gov/api/v1/amenities/parksplaces?q=tra
 //         getWeatherparks(weatherURL);}
 //  } );
 
-
+// function to grab the 5 day forecast for the national park that is selected
 function getWeatherparks(weatherURL) {
+    var weatherURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + 37.2982 + "&lon=" + -113.0263 + "&appid=" + APIkeyopenweather;
     fetch(weatherURL)
     .then(function(response) {
         return response.json();
     })
     .then(function(data) {
-        // created variables for name of park, date, weather icon, temperature, humidity, wind speed, and UV index
-        var parkName = data.city.name;
+        // created variables for name of park, date, weather icon, temperature, humidity, wind speed, and UV index to grab a 5 day forecast
         var date = data.list[0].dt_txt;
+        date= date.split(" ");
+        date= date[0];
         var weatherIcon = data.list[0].weather[0].icon;
-        var temp = data.list[0].main.temp;
+        var temperature = data.list[0].main.temp;
+        temperature = (temperature - 273.15) * 1.80 + 32;
+        temperature = Math.round(temperature);
         var humidity = data.list[0].main.humidity;
         var windSpeed = data.list[0].wind.speed;
-        var UVindex = data.list[0].main.uvi;
-        console.log(data)
-        console.log(parkName)
-        console.log(date)
-        console.log(weatherIcon)
-        console.log(temp)
-        console.log(humidity)
-        console.log(windSpeed)
-        console.log(UVindex)
-        // crea
+        windSpeed = Math.round(windSpeed);
+        // console logged the data to make sure it was working
+        console.log(data);
+        console.log(date);
+        console.log(weatherIcon);
+        console.log(temperature);
+        console.log(humidity);
+        console.log(windSpeed);
+        // created elements to display the 5 day forecast 
+        var dateEl = document.createElement("p");
+        dateEl.textContent = date;
+        fivedayforecast1El.appendChild(dateEl);
+        var weatherIconEl = document.createElement("img");
+        weatherIconEl.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon + ".png");
+        fivedayforecast1El.appendChild(weatherIconEl);
+        var temperatureEl = document.createElement("p");
+        temperatureEl.textContent = "Temperature: " + temperature + "°F";
+        fivedayforecast1El.appendChild(temperatureEl);
+        var humidityEl = document.createElement("p");
+        humidityEl.textContent = "Humidity: " + humidity + "%";
+        fivedayforecast1El.appendChild(humidityEl);
+        var windSpeedEl = document.createElement("p");
+        windSpeedEl.textContent = "Wind Speed: " + windSpeed + " MPH";
+        fivedayforecast1El.appendChild(windSpeedEl);
+        fivedayforecast1El.setAttribute("style", "background-color: rgb(60, 204, 253); border: 1px solid black; border-radius: 5px; padding: 10px; margin: 10px;");
+        //created variables for day 2 of the 5 day forecast
+        var date2 = data.list[8].dt_txt;
+        date2 = date2.split(" ");
+        date2 = date2[0];
+        var weatherIcon2 = data.list[8].weather[0].icon;
+        var temperature2 = data.list[8].main.temp;
+        temperature2 = (temperature2 - 273.15) * 1.80 + 32;
+        temperature2 = Math.round(temperature2);
+        var humidity2 = data.list[8].main.humidity;
+        var windSpeed2 = data.list[8].wind.speed;
+        windSpeed2 = Math.round(windSpeed2);
+        // created elements to display day 2 of the 5 day forecast
+        var date2El = document.createElement("p");
+        date2El.textContent = date2;
+        fivedayforecast2El.appendChild(date2El);
+        var weatherIcon2El = document.createElement("img");
+        weatherIcon2El.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon2 + ".png");
+        fivedayforecast2El.appendChild(weatherIcon2El);
+        var temperature2El = document.createElement("p");
+        temperature2El.textContent = "Temperature: " + temperature2 + "°F";
+        fivedayforecast2El.appendChild(temperature2El);
+        var humidity2El = document.createElement("p");
+        humidity2El.textContent = "Humidity: " + humidity2 + "%";
+        fivedayforecast2El.appendChild(humidity2El);
+        var windSpeed2El = document.createElement("p");
+        windSpeed2El.textContent = "Wind Speed: " + windSpeed2 + " MPH";
+        fivedayforecast2El.appendChild(windSpeed2El);
+        fivedayforecast2El.setAttribute("style", "background-color: rgb(60, 204, 253); border: 1px solid black; border-radius: 5px; padding: 10px; margin: 10px;");
+        //created variables for day 3 of the 5 day forecast
+        var date3 = data.list[16].dt_txt;
+        date3 = date3.split(" ");
+        date3 = date3[0];
+        var weatherIcon3 = data.list[16].weather[0].icon;
+        var temperature3 = data.list[16].main.temp;
+        temperature3 = (temperature3 - 273.15) * 1.80 + 32;
+        temperature3 = Math.round(temperature3);
+        var humidity3 = data.list[16].main.humidity;
+        var windSpeed3 = data.list[16].wind.speed;
+        windSpeed3 = Math.round(windSpeed3);
+        // created elements to display day 3 of 5 day forecast
+        var date3El = document.createElement("p");
+        date3El.textContent = date3;
+        fivedayforecast3El.appendChild(date3El);
+        var weatherIcon3El = document.createElement("img");
+        weatherIcon3El.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon3 + ".png");
+        fivedayforecast3El.appendChild(weatherIcon3El);
+        var temperature3El = document.createElement("p");
+        temperature3El.textContent = "Temperature: " + temperature3 + "°F";
+        fivedayforecast3El.appendChild(temperature3El);
+        var humidity3El = document.createElement("p");
+        humidity3El.textContent = "Humidity: " + humidity3 + "%";
+        fivedayforecast3El.appendChild(humidity3El);
+        var windSpeed3El = document.createElement("p");
+        windSpeed3El.textContent = "Wind Speed: " + windSpeed3 + " MPH";
+        fivedayforecast3El.appendChild(windSpeed3El);
+        fivedayforecast3El.setAttribute("style", "background-color: rgb(60, 204, 253); border: 1px solid black; border-radius: 5px; padding: 10px; margin: 10px;");
+        //created variables for day 4 of the 5 day forecast
+        var date4 = data.list[24].dt_txt;
+        date4 = date4.split(" ");
+        date4 = date4[0];
+        var weatherIcon4 = data.list[24].weather[0].icon;
+        var temperature4 = data.list[24].main.temp;
+        temperature4 = (temperature4 - 273.15) * 1.80 + 32;
+        temperature4 = Math.round(temperature4);
+        var humidity4 = data.list[24].main.humidity;
+        var windSpeed4 = data.list[24].wind.speed;
+        windSpeed4 = Math.round(windSpeed4);
+        // created elements to display day 4 of 5 day forecast
+        var date4El = document.createElement("p");
+        date4El.textContent = date4;
+        fivedayforecast4El.appendChild(date4El);
+        var weatherIcon4El = document.createElement("img");
+        weatherIcon4El.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon4 + ".png");
+        fivedayforecast4El.appendChild(weatherIcon4El);
+        var temperature4El = document.createElement("p");
+        temperature4El.textContent = "Temperature: " + temperature4 + "°F";
+        fivedayforecast4El.appendChild(temperature4El);
+        var humidity4El = document.createElement("p");
+        humidity4El.textContent = "Humidity: " + humidity4 + "%";
+        fivedayforecast4El.appendChild(humidity4El);
+        var windSpeed4El = document.createElement("p");
+        windSpeed4El.textContent = "Wind Speed: " + windSpeed4 + " MPH";
+        fivedayforecast4El.appendChild(windSpeed4El);
+        fivedayforecast4El.setAttribute("style", "background-color: rgb(60, 204, 253); border: 1px solid black; border-radius: 5px; padding: 10px; margin: 10px;");
+        //created variables for day 5 of the 5 day forecast
+        var date5 = data.list[32].dt_txt;
+        date5 = date5.split(" ");
+        date5 = date5[0];
+        var weatherIcon5 = data.list[32].weather[0].icon;
+        var temperature5 = data.list[32].main.temp;
+        temperature5 = (temperature5 - 273.15) * 1.80 + 32;
+        temperature5 = Math.round(temperature5);
+        var humidity5 = data.list[32].main.humidity;
+        var windSpeed5 = data.list[32].wind.speed;
+        windSpeed5 = Math.round(windSpeed5);
+        // created elements to display day 5 of 5 day forecast
+        var date5El = document.createElement("p");
+        date5El.textContent = date5;
+        fivedayforecast5El.appendChild(date5El);
+        var weatherIcon5El = document.createElement("img");
+        weatherIcon5El.setAttribute("src", "http://openweathermap.org/img/w/" + weatherIcon5 + ".png");
+        fivedayforecast5El.appendChild(weatherIcon5El);
+        var temperature5El = document.createElement("p");
+        temperature5El.textContent = "Temperature: " + temperature5 + "°F";
+        fivedayforecast5El.appendChild(temperature5El);
+        var humidity5El = document.createElement("p");
+        humidity5El.textContent = "Humidity: " + humidity5 + "%";
+        fivedayforecast5El.appendChild(humidity5El);
+        var windSpeed5El = document.createElement("p");
+        windSpeed5El.textContent = "Wind Speed: " + windSpeed5 + " MPH";
+        fivedayforecast5El.appendChild(windSpeed5El);
+        fivedayforecast5El.setAttribute("style", "background-color: rgb(60, 204, 253); border: 1px solid black; border-radius: 5px; padding: 10px; margin: 10px;");
+
     })     
     }
     
@@ -144,3 +279,5 @@ trailSubmit.addEventListener("click", function(event){
   selectedTrail = trailSelectEl.options[trailSelectEl.selectedIndex].value;
   console.log(selectedTrail);
   });
+
+getWeatherparks();
